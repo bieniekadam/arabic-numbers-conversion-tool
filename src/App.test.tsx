f8a83bdc1ab92129eadb873617bde1bf531dfae3
bieniekadam <adam8ieniek@gmail.com>
2022-03-01 10:14:13 +0100

    final algorythmic solution

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App, { numToWords } from "./App";
 
-test('renders learn react link', () => {
+test("displays 7 correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(numToWords(7)).toEqual("seven");
+});
+
+test("displays 42 correctly", () => {
+  render(<App />);
+  expect(numToWords(42)).toEqual("forty-two");
+});
+
+test("displays 1999 correctly", () => {
+  render(<App />);
+  expect(numToWords(1999)).toEqual("one thousand nine hundred and ninety-nine");
+});
+
+test("displays 2001 correctly", () => {
+  render(<App />);
+  expect(numToWords(2001)).toEqual("two thousand and one");
+});
+
+test("displays 17999 correctly", () => {
+  render(<App />);
+  expect(numToWords(17999)).toEqual(
+    "seventeen thousand nine hundred and ninety-nine"
+  );
+});
+
+test("displays 342251 correctly", () => {
+  render(<App />);
+  expect(numToWords(342251)).toEqual(
+    "three hundred and forty-two thousand two hundred and fifty-one"
+  );
+});
+
+test("displays 1300420 correctly", () => {
+  render(<App />);
+  expect(numToWords(1300420)).toEqual(
+    "one million three hundred thousand four hundred and twenty"
+  );
 });
